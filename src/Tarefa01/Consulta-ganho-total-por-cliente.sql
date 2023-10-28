5fcb0ef438d4b263cefe0d280292df9cc6d80bc2
rahfaeu <rahfaeu@gmail.com>
2023-10-28 10:43:41 -0300

    :sparkles: delivering 1ª task - Query earnings per customer

---
+++ src/Tarefa01/Consulta-ganho-total-por-cliente.sql
@@ -0,0 +1,18 @@
+select
+    cliente.nome as cliente_nome
+    , round(
+        sum(
+            (
+                contrato.percentual * (
+                    transacao.valor_total *(1 - isnull(transacao.percentual_desconto, 0) / 100)
+                )
+            ) / 100
+        
+        )
+        , 2
+    ) as valor
+from contrato
+    inner join transacao as transacao on(contrato.contrato_id = transacao.contrato_id)
+    inner join cliente on(contrato.cliente_id = cliente.cliente_id)
+where contrato.ativo = 'true'
+group by cliente.nome
