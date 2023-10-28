830e08d6996c58aea9ba98a2e72192005c67ea13
rahfaeu <rahfaeu@gmail.com>
2023-10-28 10:49:56 -0300

    :hammer: Query formatting

--- src/Tarefa01/Consulta-ganho-total-por-cliente.sql
+++ src/Tarefa01/Consulta-ganho-total-por-cliente.sql
@@ -1,18 +1,19 @@
-select
+select 
     cliente.nome as cliente_nome
     , round(
         sum(
             (
                 contrato.percentual * (
-                    transacao.valor_total *(1 - isnull(transacao.percentual_desconto, 0) / 100)
+                    transacao.valor_total *(
+                        1 - isnull(transacao.percentual_desconto, 0) / 100
+                    )
                 )
             ) / 100
-        
         )
         , 2
     ) as valor
 from contrato
-    inner join transacao as transacao on(contrato.contrato_id = transacao.contrato_id)
-    inner join cliente on(contrato.cliente_id = cliente.cliente_id)
+    inner join transacao on contrato.contrato_id = transacao.contrato_id
+    inner join cliente on contrato.cliente_id = cliente.cliente_id
 where contrato.ativo = 'true'
 group by cliente.nome
